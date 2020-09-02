8c0e6bb3194b4b828f21a8325311c13fa7e1ebf7
Haris <muhamad_haris@hotmail.com>
2020-09-02 20:05:46 +0800

    Add buttons settings section

---
+++ lib/settings_page/button_settings_section.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:links_landing_page/constants.dart';
+
+class ButtonSettingsSection extends StatelessWidget {
+  const ButtonSettingsSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 3,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          return Container(
+            color: Colors.blueGrey.shade50,
+            child: Column(
+              children: [
+                SizedBox(height: 40),
+                Text(
+                  'Your Links',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                SizedBox(height: 100),
+                SizedBox(
+                  width: constraints.maxWidth * 0.6,
+                  child: FlatButton(
+                    padding: EdgeInsets.symmetric(vertical: 25),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Text('Add button'),
+                    color: Colors.greenAccent.shade400,
+                    onPressed: () {},
+                  ),
+                ),
+                SizedBox(height: 30),
+                Expanded(
+                  child: SizedBox(
+                    width: constraints.maxWidth * 0.6,
+                    height: constraints.maxHeight * 0.5,
+                    child: ReorderableListView(
+                      children: [
+                        for (var document in documents)
+                          ListTile(
+                            contentPadding: EdgeInsets.symmetric(vertical: 8),
+                            title: Text(document.title),
+                            key: Key(document.title),
+                            leading: Icon(Icons.drag_indicator),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                    icon: Icon(Icons.edit), onPressed: () {}),
+                                IconButton(
+                                    icon: Icon(Icons.delete), onPressed: () {}),
+                              ],
+                            ),
+                          )
+                      ],
+                      onReorder: (oldIndex, newIndex) {},
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
