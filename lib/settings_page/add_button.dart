57de3e53b420a181b5983391f02857757cac2657
Haris <muhamad_haris@hotmail.com>
2020-09-07 13:35:54 +0800

    Add validation

--- lib/settings_page/add_button.dart
+++ lib/settings_page/add_button.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:links_landing_page/models/link_data.dart';
+import 'package:provider/provider.dart';
 
 class AddButton extends StatelessWidget {
   const AddButton({
@@ -10,6 +13,10 @@ class AddButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _linksCollection = Provider.of<CollectionReference>(context);
+    TextEditingController _titleTextController = TextEditingController();
+    TextEditingController _urlTextController = TextEditingController();
+    final _formKey = GlobalKey<FormState>();
     return SizedBox(
       width: width,
       child: FlatButton(
@@ -26,16 +33,25 @@ class AddButton extends StatelessWidget {
               return AlertDialog(
                 title: Text('Add new button'),
                 content: Form(
+                  key: _formKey,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
                       TextFormField(
+                        validator: (value) {
+                          return value.isEmpty ? 'Please add in a title' : null;
+                        },
+                        controller: _titleTextController,
                         decoration: InputDecoration(
                           labelText: 'Title',
                           hintText: 'Facebook',
                         ),
                       ),
                       TextFormField(
+                        validator: (value) {
+                          return value.isEmpty ? 'Please add in a url' : null;
+                        },
+                        controller: _urlTextController,
                         decoration: InputDecoration(
                           labelText: 'Link',
                           hintText: 'facebook.com/my-user-name',
@@ -46,7 +62,19 @@ class AddButton extends StatelessWidget {
                 ),
                 actions: [
                   FlatButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _linksCollection.add(
+                          LinkData(
+                            title: _titleTextController.text,
+                            url: _urlTextController.text,
+                          ).toMap(),
+                        );
+
+                        Navigator.of(context).pop();
+                        _formKey.currentState.reset();
+                      }
+                    },
                     child: Text('Add'),
                   ),
                   FlatButton(
